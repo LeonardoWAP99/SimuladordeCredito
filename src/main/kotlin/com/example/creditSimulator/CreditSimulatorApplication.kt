183cf4826f4c5c0f28de005294a53b3e74f15822
LeonardoWAP99 <leo.willers.alves@gmail.com>
2025-03-22 10:49:55 -0300

    start project

---
+++ src/main/kotlin/com/example/creditSimulator/CreditSimulatorApplication.kt
@@ -0,0 +1,12 @@
+package com.example.creditSimulator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditSimulatorApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditSimulatorApplication>(*args)
+
+}
